4d38629105ceda5066703c0062a802505d9a99fd
Jobava <jobaval10n@gmail.com>
2019-02-24 15:53:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+View your 360 pictures in 3 easy steps:
+
+1. Drop your pictures in the `pictures` folder
+2. Load them with the file upload widget at the top of the page
+3. Click anywhere on the page to advance the slides
---
+++ panorama.js
@@ -0,0 +1,61 @@
+var pictureURLs = []
+
+function slideshow(localstate={slideindex: 0, elem: null, urls: []} ) {
+	if (localstate.urls.length === 0) {
+		localstate.urls = pictureURLs;
+	}
+	if (localstate.elem === null) {
+		localstate.elem = document.getElementById("sky");
+        localstate.elem.setAttribute("src", pictureURLs[0]);
+        //localstate.elem = pictureURLs[0];
+	}
+	return {slideindex: localstate.slideindex, elem: localstate.elem, urls: localstate.urls};
+}
+
+function advanceSlide(localstate) {
+	if (localstate.slideindex === (localstate.urls.length - 1)) {
+		return slideshow({slideindex: 0, elem: localstate.elem, urls: localstate.urls});	
+	} else {
+		return slideshow({slideindex: localstate.slideindex + 1, elem: localstate.elem, urls: localstate.urls});
+	}
+}
+
+function rewindSlide(localstate) {
+	if (localstate.slideindex === 0) {
+		return slideshow({slideindex: (localstate.urls.length - 1), elem: localstate.elem, urls: localstate.urls});	
+	} else {
+		return slideshow({slideindex: localstate.slideindex - 1, elem: localstate.elem, urls: localstate.urls});
+	}
+}
+
+function getURL(localstate) {
+	return localstate.urls[state.slideindex];
+}
+
+function renderCurrent(localstate) {
+	localstate.elem.setAttribute("src", getURL(state));
+}
+
+var state;
+
+document.getElementById('files').onchange = function() {
+    const fileInput = document.getElementById("files");
+
+    // files is a FileList object (similar to NodeList)
+    const files = fileInput.files;
+    let file;
+
+    // loop through files
+    for (let i = 0; i < files.length; i++) {
+        file = files[i];
+        pictureURLs.push('pictures/' + file.name);
+        alert(pictureURLs[i]);
+    }
+
+    state = slideshow();
+
+    document.onclick = function(e) {
+	    state = advanceSlide(state);
+	    renderCurrent(state);
+    }
+};
